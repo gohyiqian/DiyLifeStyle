5d2f4b9ab632d1117fbffe5ea7a689287a19bcb4
gyq765 <kenji.gohyiqian@gmail.com>
2021-08-22 05:34:45 +0800

    Added dotenv

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const app = express();
 
+require("dotenv").config();
+
 app.get("/diylifestyle", (req, res) => {
   console.log("got it");
-  res.send("Hello");
+  res.send("Hello World");
   //   res.render("homepage.ejs");
 });
 
 app.listen(process.env.PORT, () => {
-  console.log("Server is listening on port");
+  console.log("Server is listening on port " + process.env.PORT);
 });
