48d6a5c4c5a80c36247ad1627a8a4187f9d9450b
gyq765 <kenji.gohyiqian@gmail.com>
2021-08-23 23:10:14 +0800

    add session

--- server.js
+++ server.js
@@ -1,9 +1,23 @@
 const express = require("express");
 const app = express();
+const session = require("express-session");
 
 require("dotenv").config();
 
 app.use(express.static("public"));
+
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+app.use((req, res, next) => {
+  res.locals.username = req.session.username;
+  next();
+});
 
 app.get("/diylifestyle", (req, res) => {
   console.log("got it");
