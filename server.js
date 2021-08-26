5e0189ae8c744103486d31a2ab81359f7d8a5314
gyq765 <kenji.gohyiqian@gmail.com>
2021-08-26 17:07:13 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,27 @@
+// DEPENDENCIES
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const db = mongoose.connection;
+const methodOverride = require("method-override");
 const session = require("express-session");
-
 require("dotenv").config();
 
+// CONGFIG
+const mongoURI = "mongodb://localhost:27017/storageWeb";
+const port = 3000;
+
+// MODELS
+
+// ERROR/SUCCESS
+db.on("error", (err) => console.log(err.message + " is Mongod not running?"));
+db.on("connected", () => console.log("mongo connected: ", mongoURI));
+db.on("disconnected", () => console.log("mongo disconnected"));
+
+// MIDDLEWARE
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 
 app.use(
   session({
@@ -19,11 +36,22 @@ app.use((req, res, next) => {
   next();
 });
 
+// GET
 app.get("/diylifestyle", (req, res) => {
   console.log("got it");
   res.render("homepage.ejs");
 });
 
-app.listen(process.env.PORT, () => {
-  console.log("Server is listening on port " + process.env.PORT);
+// LISTEN
+const server = app.listen(process.env.PORT, () => {
+  console.log(`Server is listening on port: ${process.env.PORT}`);
+});
+
+// GRACEFUL SHUTDOWN
+process.on("SIGTERM", () => {
+  console.log("Process is exiting...");
+
+  server.close(() => {
+    dbConnection.close();
+  });
 });
