6a4fd603265c545d85c512efd042882cf7a5428f
gyq765 <kenji.gohyiqian@gmail.com>
2021-08-28 08:06:06 +0800

    Added background img

--- server.js
+++ server.js
@@ -2,7 +2,6 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-
 const methodOverride = require("method-override");
 const session = require("express-session");
 require("dotenv").config();
